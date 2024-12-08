db96fe369b7f320053f73a0397510916b9923f04
gabashvili <nika2005nika2005@gmail.com>
2024-12-08 22:49:46 +0400

    added binding

--- app/src/main/java/com/example/tbcacademyhomework/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademyhomework/MainActivity.kt
@@ -5,13 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import com.example.tbcacademyhomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        handleEdgeToEdge()
 
 
+    }
 
+    private fun handleEdgeToEdge() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            val imeInsets = insets.getInsets(WindowInsetsCompat.Type.ime())
+            val bottomPadding = if (imeInsets.bottom == 0) systemBars.bottom else imeInsets.bottom
+            view.updatePadding(
+                left = systemBars.left,
+                top = systemBars.top,
+                right = systemBars.right,
+                bottom = bottomPadding
+            )
+            insets
+        }
     }
 }
