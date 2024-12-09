0f6bc782484d8a9820c864d0ad89ea7304adce5a
gabashvili <nika2005nika2005@gmail.com>
2024-12-09 09:26:21 +0400

    first part finished

---
+++ app/src/main/java/com/example/tbcacademyhomework/AnagramDatabase.kt
@@ -0,0 +1,37 @@
+package com.example.tbcacademyhomework
+
+class AnagramDatabase {
+
+    private val anagrams = mutableListOf<String>()
+    private val anagramMap = mutableMapOf<String, List<String>>()
+
+    fun addAnagram(anagram: String) {
+        anagrams.add(anagram)
+        anagramMap[formatAnagram(anagram)] = getListForAnagram(anagram) + anagram
+    }
+
+    private fun getListForAnagram(anagram: String): List<String> {
+        val formattedAnagram = formatAnagram(anagram)
+        return anagramMap[formattedAnagram] ?: emptyList()
+    }
+
+    private fun formatAnagram(anagram: String): String {
+        return anagram.toCharArray().sorted().joinToString()
+    }
+
+    fun anagramAlreadyAdded(anagram: String): Boolean {
+        return anagrams.contains(anagram)
+    }
+
+    fun getAnagrams(): String {
+        val strBuilder = StringBuilder()
+        anagramMap.values.forEachIndexed { index, anagramList ->
+            strBuilder.append("${index + 1} :")
+            strBuilder.append(anagramList)
+            strBuilder.append("\n")
+        }
+        return strBuilder.toString()
+    }
+
+
+}
--- app/src/main/java/com/example/tbcacademyhomework/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademyhomework/MainActivity.kt
@@ -1,23 +1,76 @@
 package com.example.tbcacademyhomework
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 import androidx.core.view.updatePadding
+import androidx.core.widget.doOnTextChanged
 import com.example.tbcacademyhomework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val anagramDatabase = AnagramDatabase()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         handleEdgeToEdge()
-
+        initListeners()
 
     }
+
+    private fun initListeners() {
+
+        binding.btnSave.setOnClickListener {
+            saveAnagram()
+        }
+
+        binding.etAnagramInput.doOnTextChanged { _, _, _, _ ->
+            binding.tvAnagramInputError.visibility = View.GONE
+        }
+
+        binding.btnOutput.setOnClickListener {
+            displayAnagrams()
+        }
+
+    }
+
+    private fun displayAnagrams() {
+        binding.tvAnagramInputError.visibility =
+            View.GONE //setting visibility gone in case it was still visible until this point
+        val anagrams = anagramDatabase.getAnagrams()
+        binding.tvAnagrams.text = anagrams
+    }
+
+    private fun saveAnagram() {
+        val anagram = binding.etAnagramInput.text.toString()
+
+        binding.tvAnagramInputError.isVisible = anagram.isBlank()
+        if (anagram.isBlank()) {
+            binding.tvAnagramInputError.text = getString(R.string.field_must_not_be_empty)
+        } else {
+
+            if (anagramDatabase.anagramAlreadyAdded(anagram)) {
+                makeToast(getString(R.string.anagram_already_exists))
+            } else {
+                anagramDatabase.addAnagram(anagram)
+                binding.etAnagramInput.text?.clear()
+                makeToast(getString(R.string.anagram_added_successfully))
+            }
+
+        }
+
+    }
+
+    private fun makeToast(toastMessage: String) {
+        Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show()
+    }
+
 
     private fun handleEdgeToEdge() {
         enableEdgeToEdge()
