6df96209e6d572b22b71340ee33ed712886165be
gabashvili <nika2005nika2005@gmail.com>
2024-12-12 22:13:35 +0400

    finished

--- app/src/main/java/com/example/tbcacademyhomework/MainActivity.kt
+++ app/src/main/java/com/example/tbcacademyhomework/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tbcacademyhomework
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.AbsoluteSizeSpan
+import android.text.style.ForegroundColorSpan
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,7 +20,41 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         handleEdgeToEdge()
 
+        initScreen()
 
+
+    }
+
+    private fun initScreen() {
+        binding.tvLocationName.text = getString(R.string.location_name)
+        binding.tvLocation.text = getString(R.string.location)
+        binding.tvOverview.text = getString(R.string.overview)
+        binding.tvDetails.text = getString(R.string.details)
+        binding.tvTime.text = getString(R.string.time)
+        binding.tvTemperature.text = getString(R.string.temperature)
+        binding.tvRating.text = getString(R.string.rating)
+        binding.btnBook.text = getString(R.string.book_now)
+        binding.tvPriceKey.text = getString(R.string.price)
+        binding.tvDescription.text = getString(R.string.description)
+        setPrice(getString(R.string.price_value))
+
+    }
+
+    private fun setPrice(text: String) {
+        if (text.startsWith("$")) {
+            val spannableStringBuilder = SpannableStringBuilder(text).apply {
+                setSpan(
+                    ForegroundColorSpan(getColor(R.color.dollar_color)),
+                    0,
+                    1,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                )
+                setSpan(AbsoluteSizeSpan(20, true), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+            binding.tvPrice.text = spannableStringBuilder
+        }else{
+            binding.tvPrice.text = text
+        }
     }
 
     private fun handleEdgeToEdge() {
