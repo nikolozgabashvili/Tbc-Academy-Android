ab71387e7e06a4369aaa75f2624bfb7720a05072
gabashvili <nika2005nika2005@gmail.com>
2025-01-14 19:43:56 +0400

    added parcelize plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,9 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
 }
+
 
 buildscript {
     repositories {
