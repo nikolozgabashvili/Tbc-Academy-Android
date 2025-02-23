6c12b171606ba20d3e57cbd138666c145557b665
nikoloz gabashvili <nika2005nika2005@gmail.com>
2025-02-23 14:36:13 +0400

    added dagger hilt plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kapt) apply false
 }
 
 
