461b0e9bea0478ddb71bf94b5ad21fd5b8dace54
nikoloz gabashvili <nika2005nika2005@gmail.com>
2025-03-02 00:23:49 +0400

    added topAppbar

--- app/src/main/java/com/example/tbcacademyhomework/domain/managers/theme/Theme.kt
+++ app/src/main/java/com/example/tbcacademyhomework/domain/managers/theme/Theme.kt
@@ -1,7 +1,7 @@
 package com.example.tbcacademyhomework.domain.managers.theme
 
-enum class Theme(val value: Int = 0) {
-    LIGHT(1),
-    DARK(2),
-    SYSTEM(-1)
+enum class Theme {
+    LIGHT,
+    DARK,
+    SYSTEM
 }
